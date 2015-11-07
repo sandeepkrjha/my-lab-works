e70bb30a4cad442cea77c5f125a5448623d8604d
sandeepkrjha <sandeepkjha18@gmail.com>
2015-11-07 23:38:26 +0530

    added adjacenccy list

---
+++ Graph/adjList.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct adjNode{
+	int data;
+	struct adjNode *next;
+}adjNode;
+typedef struct adjList
+{
+	struct adjNode *head;
+	
+}adjList;
+
+typedef struct Graph{
+	int V;
+	adjList *array;
+}Graph;
+adjNode *createNode(int node)
+{
+	adjNode *newNode;
+	newNode=(adjNode*)malloc(sizeof(adjNode));
+	newNode->data=node;
+	newNode->next=NULL;
+	return newNode;
+}
+//function to create a graph and initialize the each list to NULL
+Graph *createGraph(int n)//n is number of vertices
+{
+	Graph* G;
+	G=(Graph*)malloc(sizeof(Graph));
+	G->V=n;
+	G->array=(adjList*)malloc((n+1)*sizeof(adjList));
+	int i;
+	for(i=1;i<=n;++i)
+	{
+		G->array[i].head=NULL;
+	}
+	return G;
+}
+//adding edge in undirectional graph
+void addEdge(Graph* G,int node1,int node2)
+{
+	adjNode *newNode1=createNode(node1);//adding node2 in list of node1
+	newNode1->next=G->array[node2].head;
+	G->array[node2].head=newNode1;
+	adjNode* newNode2=createNode(node2);//adding node1 in list of node2
+	newNode2->next=G->array[node1].head;
+	G->array[node1].head=newNode2;
+
+
+}
+
+void printGraph(Graph* G)
+{
+	int i;
+	for(i=1;i<=G->V;++i)
+	{
+		adjNode* traver=G->array[i].head;//a traversal method to print graph
+		printf("list at node %d:: %d",i,i);
+		while(traver!=NULL)
+		{
+			printf("->%d",traver->data);
+			traver=traver->next;
+		}
+		printf("\n\n");
+	}
+}
+
+
+
+int main()
+{
+	Graph *G;
+	int V,node1,node2;
+	printf("enter the number of vertices\n");
+	scanf("%d",&V);
+	G=createGraph(V);
+	printf("enter the nodes which shares edge with each other:\n");
+	printf("enter 0, 0 to end the graph\n");
+	while(true)
+	{
+		scanf("%d%d",&node1,&node2);
+		if(node1==0 || node2==0)
+		{
+			break;
+		}
+		else
+			addEdge(G,node1,node2);
+	}
+	printGraph(G);
+
+
+}
+
